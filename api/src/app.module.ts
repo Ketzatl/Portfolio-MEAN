674cd355c5c4965e93113687d53fa2823a6cc17d
Morgan <morgan.nott@hotmail.com>
2022-09-22 19:34:45 +0200

    feat/api: Installation et configuration mongoose/mogoDB Atlas

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import {MongooseModule} from "@nestjs/mongoose";
+import config_mongoose from "./config_mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+      EventsModule,
+      MongooseModule.forRoot(config_mongoose.mongoUri, {useNewUrlParser: true}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/config_mongoose.ts
@@ -0,0 +1,7 @@
+
+export default {
+    mongoUri:
+        'mongodb+srv://tukho:PV5ZG4r3J83R9E8jfva@portfoliomean.p5vdtxk.mongodb.net/?retryWrites=true&w=majority',
+    secretKey: 'kds79po5seRT1',
+    port: 8080,
+};
